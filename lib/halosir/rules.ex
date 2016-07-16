b130ca00a66b3dce4b99276560ccb54a59d63cd6
AquarHEAD Lou <aquarhead@gmail.com>
2016-07-17 01:19:56 +0800

    Filter out long string as well
    For example in Chinese, you can select a long setence without any space in between.

--- lib/halosir/rules.ex
+++ lib/halosir/rules.ex
@@ -7,9 +7,17 @@ defmodule HaloSir.Rules do
   Don't cache sentences.
   """
   def should_cache_word?(word) do
+    not_too_long?(word) && not_sentence?(word)
+  end
+
+  defp not_sentence?(word) do
     word
     |> String.split()
     |> Kernel.length()
     |> Kernel.<=(3)
+  end
+
+  defp not_too_long?(word) do
+    String.length(word) <= 20
   end
 end
