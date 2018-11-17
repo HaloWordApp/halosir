d3a8db08f1546025d02d21a3b999b229134fef61
LOU Xun <aquarhead@ela.build>
2018-11-17 16:44:36 +0000

    Add Dockerfile for builder

---
+++ docker/builder/builder_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Prepare GitHub repo access
+mkdir /root/.ssh/
+touch /root/.ssh/known_hosts
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+# Install unzip for Elixir
+apt-get update
+apt-get install -y --no-install-recommends unzip
+
+# Install Erlang and Elixir
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.1
+
+. ~/.asdf/asdf.sh
+asdf plugin-add erlang
+asdf plugin-add elixir
+asdf install
